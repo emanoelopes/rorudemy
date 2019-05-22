817afe0959534bc8c38046b6d41648d3427cc03c
Emanoel Lopes <emanoelopes@gmail.com>
2019-05-22 17:40:48 +0000

    Add categories models and tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -21,6 +21,6 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/new'
         assert_select 'h2.panel-title'
         assert_select 'div.panel-body'
-        end
+    end
         
 end
